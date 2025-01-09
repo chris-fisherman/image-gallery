e98bfa04c45998247cc02a9fc5fd70b4836625d1
Christian Peraza <christianperaza2000@gmail.com>
2025-01-09 00:06:10 -0300

    Edit input and button
    Change input type from search to text, and edit placeholder. And add search icon into button.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -15,12 +15,16 @@ function SearchBar({ searchText, setTerm }) {
         <form className='searchForm' onSubmit={onSubmit}>
             <input 
                 className='searchForm-input'
-                type="search" 
-                name="search"
-                placeholder='Pesquise qualquer coisa...'
+                type="text" 
+                name="text"
+                placeholder='Pesquise por qualquer coisa'
                 onChange={(e) => {setText(e.target.value); setTerm(e.target.value)}}
             />
-            <button className='searchForm-button' type="submit">Pesquisar</button>
+            <button className='searchForm-button' type="submit">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                </svg>
+            </button>
         </form>
     )
 }
